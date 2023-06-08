f08e2a488da0e3e60ef1c4aafc5db95dff9d8723
exonightch <133414628+exonightch@users.noreply.github.com>
2023-06-08 17:03:22 +0200

    Add files via upload

---
+++ stream/src/config.ts
@@ -0,0 +1,13 @@
+export default {
+  //Site display name
+  SITE_TITLE: "Exos Streaming"
+
+  //Put your discord link here or null to disable
+  SITE_DISCORD: "https://dsc.gg/exo-protect",
+  
+  //True or false to show or hide site credit in footer (You should leave enabled 😉)
+  SITE_CREDITS: false,
+
+  //Do not change or site will break!
+  RIPPER_API: "https://api.riptv.net"
+}
